09b4bdc77ec475c27c5977f1b68696565399dade
andresapiro <andres16@al.insper.edu.br>
2023-05-03 14:57:17 -0300

    Primeiro codigo

--- teste.py
+++ teste.py
@@ -1,1 +1,11 @@
-print("Hello dessoft")
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    lista_posicao = [[linha, coluna]]
+    contador =1
+    for e in range(tamanho-1):
+        if orientacao == 'vertical':
+            lista_posicao.append([linha+contador, coluna])
+            contador +=1
+        else:
+            lista_posicao.append([linha, coluna+contador])
+            contador +=1
+    return lista_posicao
