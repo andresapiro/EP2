f59b537401e62f632d61f10efb621c5557e9aeb9
andresapiro <andres16@al.insper.edu.br>
2023-05-05 10:28:20 -0300

    2 e 3

--- teste.py
+++ teste.py
@@ -9,3 +9,20 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
             lista_posicao.append([linha, coluna+contador])
             contador +=1
     return lista_posicao
+
+def preenche_frota(dicionario_frota, nome_navio, linha, coluna, orientacao, tamanho):
+    lista_posicao = define_posicoes(linha, coluna, orientacao, tamanho)
+    if nome_navio not in dicionario_frota:
+        dicionario_frota[nome_navio] = [lista_posicao]
+    else:
+        dicionario_frota[nome_navio].append(lista_posicao)
+    return dicionario_frota
+
+
+def faz_jogada(tabuleiro, linha, coluna):
+    if tabuleiro[linha][coluna] == 0:
+        tabuleiro[linha][coluna] = '-'
+    
+    if tabuleiro[linha][coluna] == 1:
+        tabuleiro[linha][coluna] = 'X'
+    return tabuleiro
